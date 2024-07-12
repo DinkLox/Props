f58edd5bd4b03db55263bf85ab12f8536477e6ae
NGUYEN DINH LOC <Thenorthsea175@gmail.com>
2024-07-12 16:11:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Display from './Display.vue'
+import { ref } from 'vue'
+const res = ref([
+  {
+    id: 1,
+    title: 'aye',
+    views: 10.0,
+    author: 'Loc'
+  },
+  {
+    id: 2,
+    title: 'ayee',
+    views: 20.0,
+    author: 'Locc'
+  },
+  {
+    id: 3,
+    title: 'aaye',
+    views: 110.0,
+    author: 'LLoc'
+  }
+])
+
+console.log(res.value)
+// })
+</script>
+<template>
+  <span v-for="item in res">
+    <Display :title="item.title" :author="item.author" :views="item.views" />
+  </span>
+</template>
+<style></style>
---
+++ src/Display.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps(['title', 'author', 'views'])
+</script>
+<template>
+  <div class="container">
+    <div class="item">
+      <img src="https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg" alt="Image 1" />
+      <div class="title">{{ props.title }}</div>
+      <div class="author">{{ props.author }}</div>
+      <div class="views">{{ props.views }}</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  margin: 0;
+}
+.container {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 10px;
+  width: 80%;
+}
+.item {
+  text-align: center;
+}
+.item img {
+  width: 100%;
+  height: 100px; /* Đặt chiều cao cố định */
+  object-fit: cover; /* Giữ tỉ lệ hình ảnh */
+  display: block;
+}
+.title {
+  font-weight: bold;
+  margin: 5px 0;
+}
+.author {
+  color: gray;
+  margin: 3px 0;
+}
+.views {
+  color: darkblue;
+  margin: 3px 0;
+}
+</style>
